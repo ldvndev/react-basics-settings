9ad3415009fe34c41f22db5b13c4b49e5bbcbeee
ldvndev <ldvndev@gmail.com>
2023-12-18 01:26:08 -0300

    feat: Add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 
-export function App () {
+export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <div />
