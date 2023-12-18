53631e6f885b5a9bebef91619dce4fd01c7cf378
ldvndev <ldvndev@gmail.com>
2023-12-18 01:22:20 -0300

    feat: Add style global

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { ThemeProvider } from 'styled-components'
+
 import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App () {
   return (
     <ThemeProvider theme={defaultTheme}>
       <div />
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
